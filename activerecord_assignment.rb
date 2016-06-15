418204815ad133ad57d0b0559c7b4e6e58a196b5
Mina Mikhail <mina@fightingtheboss.com>
2016-06-15 16:15:18 -0400

    Move assignment questions to specific files and add preamble to README

---
+++ activerecord_assignment.rb
@@ -0,0 +1,46 @@
+### Active Record Query Interface Assignment
+
+### Provide one or more Active Record queries that retrieve the requested data
+### below each of the following questions:
+
+
+# 1a) Find the genre with the name "Hip Hop/Rap".
+
+
+
+# 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
+
+
+
+# 2) Find the total amount of time required to listen to all the tracks in the database.
+
+
+
+# 3a) Find the highest price of any track that has the media type "MPEG audio file".
+
+
+
+# 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
+
+
+
+# 4) Find the 2 oldest artists.
+
+
+
+### Stretch Exercises (Active Record Query Interface)
+
+### Some of these questions require the use of joins
+### http://guides.rubyonrails.org/active_record_querying.html#joining-tables
+### (see also http://www.w3schools.com/sql/sql_join.asp)
+
+
+# 1) Find all the tracks that belong to the first playlist.
+
+
+
+# 2) Write a series of queries to find all the tracks that belong to the 2 most recent playlists.
+
+
+
+# 3) Find all the albums whose titles start with B.
---
+++ sql_assignment.sql
@@ -0,0 +1,23 @@
+-- SQL Assignment
+-- Check out [W3Schools' SQL Reference](http://www.w3schools.com/sql/sql_syntax.asp) as a reference.
+
+-- Provide one or more SQL queries that retrieve the requested data
+-- below each of the following questions:
+
+-- 1) Find the albums recorded by the artist Queen.
+
+
+
+-- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
+
+
+
+-- 3) Find the least expensive track that has the genre "Electronica/Dance".
+
+
+
+-- 4) Find the all the artists whose names start with A.
+
+
+
+-- 5) Find all the tracks that belong to playlist 1.
