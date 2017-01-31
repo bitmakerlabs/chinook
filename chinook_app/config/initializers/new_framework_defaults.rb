6a3171d57b0f5494d4fd2e49bd0956ed79c0e949
Fred Ngo <fredngo@gmail.com>
2017-01-31 17:51:21 -0500

    Upgrade to Rails 5.0.1

--- chinook_app/config/initializers/new_framework_defaults.rb
+++ chinook_app/config/initializers/new_framework_defaults.rb
@@ -2,9 +2,7 @@
 #
 # This file contains migration options to ease your Rails 5.0 upgrade.
 #
-# Once upgraded flip defaults one by one to migrate to the new default.
-#
-# Read the Rails 5.0 release notes for more info on each option.
+# Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = false
